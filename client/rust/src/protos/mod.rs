db84343c6e4b35da6f351f940f22ce5b62c965c3
Michael Henkel <mhenkel@juniper.net>
2022-03-15 15:13:42 -0700

    fix

--- client/rust/src/protos/mod.rs
+++ client/rust/src/protos/mod.rs
@@ -1,56 +1,28 @@
-pub mod sigs {
-    pub mod k8s {
-        pub mod io {
-            pub mod apiserver_builder_alpha {
-                pub mod pkg {
-                    pub mod builders {
-                        include!("sigs.k8s.io.apiserver_builder_alpha.pkg.builders.rs");
-                    }
-                }
-            }
-        }
-    }
-}
-pub mod github {
-    pub mod com {
-        pub mod michaelhenkel {
-            pub mod config_controller {
-                pub mod pkg {
-                    pub mod apis {
-                        pub mod v1 {
-                            include!("github.com.michaelhenkel.config_controller.pkg.apis.v1.rs");
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
 pub mod k8s {
     pub mod io {
         pub mod apimachinery {
             pub mod pkg {
-                pub mod util {
-                    pub mod intstr {
-                        include!("k8s.io.apimachinery.pkg.util.intstr.rs");
-                    }
-                }
                 pub mod runtime {
                     pub mod schema {
                         include!("k8s.io.apimachinery.pkg.runtime.schema.rs");
                     }
                     include!("k8s.io.apimachinery.pkg.runtime.rs");
                 }
-                pub mod api {
-                    pub mod resource {
-                        include!("k8s.io.apimachinery.pkg.api.resource.rs");
-                    }
-                }
                 pub mod apis {
                     pub mod meta {
                         pub mod v1 {
                             include!("k8s.io.apimachinery.pkg.apis.meta.v1.rs");
                         }
+                    }
+                }
+                pub mod api {
+                    pub mod resource {
+                        include!("k8s.io.apimachinery.pkg.api.resource.rs");
+                    }
+                }
+                pub mod util {
+                    pub mod intstr {
+                        include!("k8s.io.apimachinery.pkg.util.intstr.rs");
                     }
                 }
             }
@@ -79,6 +51,34 @@ pub mod ssd_git {
                                 }
                             }
                         }
+                    }
+                }
+            }
+        }
+    }
+}
+pub mod github {
+    pub mod com {
+        pub mod michaelhenkel {
+            pub mod config_controller {
+                pub mod pkg {
+                    pub mod apis {
+                        pub mod v1 {
+                            include!("github.com.michaelhenkel.config_controller.pkg.apis.v1.rs");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+pub mod sigs {
+    pub mod k8s {
+        pub mod io {
+            pub mod apiserver_builder_alpha {
+                pub mod pkg {
+                    pub mod builders {
+                        include!("sigs.k8s.io.apiserver_builder_alpha.pkg.builders.rs");
                     }
                 }
             }
--- client/rust/src/resources/virtualnetwork.rs
+++ client/rust/src/resources/virtualnetwork.rs
@@ -14,8 +14,8 @@ use std::{thread, time};
 use futures;
 use futures::executor;
 use std::collections::VecDeque;
-use std::sync::{Arc, Mutex};
-//use tokio::sync::Mutex;
+use std::sync::Arc;
+use tokio::sync::Mutex;
 use crossbeam_channel::{unbounded, bounded, TryRecvError};
 use std::collections::HashMap;
 use std::vec::Vec;
@@ -29,129 +29,140 @@ impl ProcessResource for v1alpha1::VirtualNetwork {
 
 
 pub struct VirtualNetworkController {
-    receiver: crossbeam_channel::Receiver<v1::Resource>,
-    channel: tonic::transport::Channel
+    receiver: tokio::sync::mpsc::Receiver<v1::Resource>,
+    channel: tonic::transport::Channel,
+    //client: ConfigControllerClient<tonic::transport::Channel>,
 }
 
 impl VirtualNetworkController {
-    pub fn new(channel: tonic::transport::Channel, receiver: crossbeam_channel::Receiver<v1::Resource>) -> VirtualNetworkController {
-        VirtualNetworkController{
-            channel: channel,
+    pub fn new(channel: tonic::transport::Channel, receiver: tokio::sync::mpsc::Receiver<v1::Resource>) -> Self {
+        //let client = ConfigControllerClient::new(channel.clone());
+        Self{
             receiver: receiver,
+            //client: client,
+            channel: channel,
         }
     }
-    pub async fn run(&mut self) -> Result<(), Box<dyn Error>> {
+    pub async fn run(mut self) -> Result<(), Box<dyn Error>> {
         println!("starting virtual_network_controller");
-        let (mut consumer_sender, mut consumer_receiver): (crossbeam_channel::Sender<v1::Resource>, crossbeam_channel::Receiver<v1::Resource>) = bounded(2);        
-        let queue_watcher_thread = queue_watcher(&mut self.receiver, &mut consumer_sender);
-        //let resource_consumer_thread = resource_consumer(&mut consumer_receiver);
-        futures::join!(queue_watcher_thread);
+
+        tokio::spawn(async move {
+            let _queue_watcher_thread = self.queue_watcher().await;
+        });
+
         Ok(())
     }
-}
 
-pub async fn queue_watcher(receiver: &mut crossbeam_channel::Receiver<v1::Resource>, consumer_sender: &mut crossbeam_channel::Sender<v1::Resource>) {
-    println!("starting queue_watcher");
-    let receiver_clone = receiver.clone();
-    let sender_clone = consumer_sender.clone();
-    let mut resource_queue: VecDeque<v1::Resource> = VecDeque::new();
-
-    
-    let _guard = thread::spawn(move || {
+    pub async fn queue_watcher(&mut self) {
+        println!("starting queue_watcher");
         let worker_queue: Vec<v1::Resource> = Vec::new();
         let worker_queue_mutex = Arc::new(Mutex::new(worker_queue));
-
-        loop{
-            match receiver_clone.try_recv() {
-                Ok(resource) => { 
-                    println!("received resource 2 {:?}", resource);
-                    if !resource_queue.contains(&resource){
-                        resource_queue.push_back(resource);
-                    }
-                },
-                Err(TryRecvError::Empty) => {
-                    if !resource_queue.is_empty(){
-                        let resource = resource_queue.pop_front().unwrap();
-                        let worker_queue_clone = worker_queue_mutex.clone();
-                        let mut worker_queue_lock = worker_queue_mutex.lock().unwrap();
-                        if !worker_queue_lock.contains(&resource){
-                            worker_queue_lock.push(resource.clone());
-                            println!("found resource {:?} in queue", resource.clone());
-                            let _join_handler = thread::spawn(move || {
-                                println!("consuming {:?} for 30 seconds", resource.clone());
-                                thread::sleep(time::Duration::from_secs(30));
-                                println!("done");
-                                let mut worker_queue_lock = worker_queue_clone.lock().unwrap();
-                                worker_queue_lock.retain(|x| *x != resource.clone());
-                            });
-                        } else {
-                            if !resource_queue.contains(&resource){
-                                resource_queue.push_back(resource);
-                            }
-                            thread::sleep(time::Duration::from_millis(1));
-                        }
-                    } else {
-                        thread::sleep(time::Duration::from_millis(1));
-                        continue;
-                    }
-                },
-                _ => { continue; },
+        let mut resource_queue: VecDeque<v1::Resource> = VecDeque::new();
+        let client = ConfigControllerClient::new(self.channel.clone());
+        loop {
+            let resource = self.receiver.recv().await.unwrap();
+            let mut worker_queue_lock = worker_queue_mutex.lock().await;
+            let worker_queue_clone = worker_queue_mutex.clone();
+            if !worker_queue_lock.contains(&resource){
+                worker_queue_lock.push(resource.clone());
+                println!("got {:?}", resource);
+                let mut client = client.clone();
+                tokio::spawn(async move {
+                    get_resource(&mut client, resource.clone()).await;
+                    let mut worker_queue_lock = worker_queue_clone.lock().await;
+                    worker_queue_lock.retain(|x| *x != resource.clone());
+                });
+            } else {
+                if !resource_queue.contains(&resource){
+                    resource_queue.push_back(resource);
+                }
             }
         }
-    });
+        
+    }
 }
 
-pub async fn resource_consumer(receiver: &mut crossbeam_channel::Receiver<v1::Resource>) {
-    //let mut join_handler_map: HashMap<v1::Resource,std::thread::JoinHandle<()>> = HashMap::new();
-    //let mut join_vec: Vec<std::thread::JoinHandle<()> = Vec::new();
-    let receiver_clone = receiver.clone();
-    let _guard = thread::spawn(move || {
-        loop {
-            let resource = receiver_clone.recv().unwrap();
-            let _join_handler = thread::spawn(move || {
-                println!("consuming {:?} for 30 seconds", resource.clone());
-                thread::sleep(time::Duration::from_secs(30));
-                println!()
-            });
-
+pub async fn process(worker_queue_mutex: Arc<Mutex<Vec<v1::Resource>>>, resource: v1::Resource, client: &mut ConfigControllerClient<tonic::transport::Channel>, resource_queue: &mut VecDeque<v1::Resource> ) {
+    let mut worker_queue_lock = worker_queue_mutex.lock().await;
+    let worker_queue_clone = worker_queue_mutex.clone();
+    if !worker_queue_lock.contains(&resource){
+        worker_queue_lock.push(resource.clone());
+        println!("got {:?}", resource);
+        let mut client = client.clone();
+        tokio::spawn(async move {
+            get_resource(&mut client, resource.clone()).await;
+            let mut worker_queue_lock = worker_queue_clone.lock().await;
+            worker_queue_lock.retain(|x| *x != resource.clone());
+        });
+    } else {
+        if !resource_queue.contains(&resource){
+            resource_queue.push_back(resource);
         }
-    });
+    }
+}
+
+pub async fn get_resource(client: &mut ConfigControllerClient<tonic::transport::Channel>, resource: v1::Resource){
+    let mut client = client.clone();
+    let res_result: Result<tonic::Response<v1alpha1::VirtualNetwork>, tonic::Status> = client.get_virtual_network(resource).await;
+    let res_resp: &mut tonic::Response<v1alpha1::VirtualNetwork> = &mut res_result.unwrap();
+    let res: &mut v1alpha1::VirtualNetwork = res_resp.get_mut();
+    println!("{}/{}", res.metadata.as_ref().unwrap().namespace(), res.metadata.as_ref().unwrap().name());
+    println!("labels {:?}", res.metadata.as_ref().unwrap().labels);
+    println!("sleeping for 30 sec");
+    tokio::time::sleep(tokio::time::Duration::from_secs(30)).await;
+    println!("done");
 }
 
 /*
-    let worker_queue: VecDeque<v1::Resource> = VecDeque::new();
-    let worker_queue_mutex = Arc::new(Mutex::new(worker_queue));
-    
-    tokio::spawn(async move {
-        loop {
-           
-            let mut resource_queue_lock = resource_queue_mutex_clone.lock().await;
-            let worker_queue_lock = worker_queue_mutex.lock().await;
-            if resource_queue_lock.is_empty() {
-                continue;
+    pub async fn queue_watcher(&mut self) {
+        println!("starting queue_watcher");
+        let client = ConfigControllerClient::new(self.channel.clone());
+        let receiver = self.receiver.clone();
+        let mut resource_queue: VecDeque<v1::Resource> = VecDeque::new();    
+        let _guard = tokio::spawn(async move {
+            let worker_queue: Vec<v1::Resource> = Vec::new();
+            let worker_queue_mutex = Arc::new(Mutex::new(worker_queue));
+            loop{
+                match receiver.try_recv() {
+                    Ok(resource) => { 
+                        println!("received resource 2 {:?}", resource);
+                        if !resource_queue.contains(&resource){
+                            resource_queue.push_back(resource);
+                        }
+                    },
+                    Err(TryRecvError::Empty) => {
+                        if !resource_queue.is_empty(){
+                            let resource = resource_queue.pop_front().unwrap();
+                            let worker_queue_clone = worker_queue_mutex.clone();
+                            let mut worker_queue_lock = worker_queue_mutex.lock().unwrap();
+                            if !worker_queue_lock.contains(&resource){
+                                worker_queue_lock.push(resource.clone());
+                                println!("found resource {:?} in queue", resource.clone());
+                                let mut client = client.clone();
+                                let _join_handler = tokio::spawn(async move {
+                                    println!("consuming {:?} for 30 seconds", resource.clone());
+                                    /*
+                                    let result = get_resource(&mut client, resource.clone()).await;
+                                    thread::sleep(time::Duration::from_secs(30));
+                                    println!("done");
+                                    let mut worker_queue_lock = worker_queue_clone.lock().unwrap();
+                                    worker_queue_lock.retain(|x| *x != resource.clone());
+                                    */
+                                });
+                            } else {
+                                if !resource_queue.contains(&resource){
+                                    resource_queue.push_back(resource);
+                                }
+                                thread::sleep(time::Duration::from_millis(1));
+                            }
+                        } else {
+                            thread::sleep(time::Duration::from_millis(1));
+                            continue;
+                        }
+                    },
+                    _ => { continue; },
+                }
             }
-            let resource = resource_queue_lock.pop_front().unwrap();
-            println!("sending resource {:?} to consumer", resource.clone()); 
-
-            
-            if worker_queue_lock.len() < 2 && !worker_queue_lock.contains(&resource.clone()) {
-                let worker_queue_mutex_clone = Arc::clone(&resource_queue_mutex_clone);
-                
-                tokio::spawn(async move {
-                    let mut worker_queue_mutex_clone_lock = worker_queue_mutex_clone.lock().await;
-                    worker_queue_mutex_clone_lock.push_back(resource.clone());
-                    let mut cloned_resource = resource.clone();
-                    let result = resource_consumer(&mut cloned_resource);
-                    worker_queue_mutex_clone_lock.pop_front();
-                    println!("done");
-                });
-                
-                
-            } else {
-                println!("worker queue full or resource is currently processed, pushing back");
-                resource_queue_lock.push_back(resource.clone());
-            }
-            //consumer_sender_clone.send(resource.clone()).await;
-        }
-    });
-    */
+        });
+    }
+*/
